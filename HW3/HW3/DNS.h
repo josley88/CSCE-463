e955ee45286f3b4e95b66af0eaeb0d67dd3f93ad
Joseph Shumway <joseph.b.shumway@gmail.com>
2023-02-08 20:11:44 -0600

    Added HW3

---
+++ HW3/HW3/DNS.h
@@ -0,0 +1,13 @@
+/* DNS.h
+ * Joseph Shumway
+ * CSCE 463
+ * Spring 2023
+ */
+
+#pragma once
+#include <Iphlpapi.h>
+
+class DNS {
+public:
+	void printDNSServer (void);
+};
---
+++ HW3/HW3/Utility.h
@@ -0,0 +1,13 @@
+/* Utility.h
+ * Joseph Shumway
+ * CSCE 463
+ * Spring 2023
+ */
+
+#pragma once
+namespace Utility {
+	
+	int getURLs(int argc, char** argv, char*** URLs);
+	void cleanQuit();
+};
+
